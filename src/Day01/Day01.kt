ce5276860eaa3263a9a9327311f2ddb8e5c2bb51
Nathan Molby <nathan.molby@willowtreeapps.com>
2022-12-01 07:48:55 -0500

    Getting fancy readme set up

--- src/Day01/Day01.kt
+++ src/Day01/Day01.kt
@@ -1,3 +1,7 @@
+package Day01
+
+import readInput
+
 fun main() {
     class Elf(var calories: MutableList<Int>) {
         fun addCalorie(calorie: Int) {
@@ -40,10 +44,10 @@ fun main() {
 
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
+    val testInput = readInput("Day01","Day01_test")
     check(part1(testInput) == 24000)
 
-    val input = readInput("Day01")
+    val input = readInput("Day01","Day01")
     println(part1(input))
     println(part2(input))
 }
